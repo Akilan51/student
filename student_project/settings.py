04d049dcf405bd169eb0757cf1475d3c6493e3cb
akilanakilan832-spec <akilanakilan832@gmail.com>
2026-09-05 14:53:25 +0530

    add new update

--- student_project/settings.py
+++ student_project/settings.py
@@ -22,16 +22,17 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ.get(
-    "SECRET_KEY"
-    'django-development-key'
+    "SECRET_KEY",
+    "django-insecure-development-key"
 )
 
+# SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get("DEBUG", "False") == "True"
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = ['*']
 
 CSRF_TRUSTED_ORIGINS = [
-    "https://web-production-a6ad2.up.railway.app",
+    "https://web-production-a6ad2.up.railway.app"
 ]
 
 
